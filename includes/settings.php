f3e1a7a6f8f246a6fb5eadecf5eb5d466636bb95
Myros <miroslav.milak@gmail.com>
2020-12-24 00:14:28 -0500

    Activated debug option

--- includes/settings.php
+++ includes/settings.php
@@ -13,13 +13,21 @@ namespace RemoteCachePurger;
 class Settings {
   const NAME = 'remote-cache-purger-settings';
 
+  private $main = null;
   private $plugin = null;
+  private $prefix = null;
 
   /**
    * @since 1.0.1
   */
   public function __construct() {
-    $this->plugin = Main::getInstance();
+    $this->main = Main::getInstance();
+    $this->plugin = $this->main->plugin;
+    $this->prefix = $this->main->prefix;
+
+    $this->admin_menu();
+    
+    $this->currentTab = isset($_GET['tab']) ? $_GET['tab'] : 'settings';
 
     add_filter('admin_footer_text', array( &$this, 'admin_footer' ), 1, 2 );
   }
@@ -51,7 +59,178 @@ class Settings {
 
   }
 
-  /**
+    /**
+    * @since 1.0
+    */
+    public function admin_menu()
+    {
+        add_action('admin_menu', array($this, 'add_menu_item'));
+        add_action('admin_init', array($this, 'options_page_fields'));
+        add_action('admin_init', array($this, 'console_page_fields'));
+    }
+
+    /**
+     * @since 1.0
+    */
+    public function add_menu_item()
+    {
+        if ($this->main->check_if_purgeable()) {
+            add_menu_page(__('Remote Cache Purger', $this->plugin), __('Remote Cache Purger', $this->plugin), 'manage_options', $this->plugin . '-plugin', array($this, 'settings_page'), 'dashicons-sos', 99);
+        }
+    }
+
+    /**
+    * @since 1.0
+    */
+    public function settings_page()
+    {
+    ?>
+        <div class="wrap">
+        <h1><?=__('Remote Cache Purger', $this->plugin)?></h1>
+
+        <h2 class="nav-tab-wrapper">
+            <a class="nav-tab <?php if($this->currentTab == 'settings'): ?>nav-tab-active<?php endif; ?>" href="<?php echo admin_url() ?>index.php?page=<?=$this->plugin?>-plugin&amp;tab=settings"><?=__('Settings', $this->plugin)?></a>
+            <?php if ($this->main->check_if_purgeable()): ?>
+                <a class="nav-tab <?php if($this->currentTab == 'console'): ?>nav-tab-active<?php endif; ?>" href="<?php echo admin_url() ?>index.php?page=<?=$this->plugin?>-plugin&amp;tab=console"><?=__('Console', $this->plugin)?></a>
+            <?php endif; ?>
+        </h2>
+
+        <?php if($this->currentTab == 'settings'): ?>
+            <form method="post" action="options.php">
+                <?php
+                    settings_fields($this->prefix . 'settings');
+                    do_settings_sections($this->prefix . 'settings');
+                    submit_button();
+                ?>
+            </form>
+        <?php elseif($this->currentTab == 'console'): ?>
+            <form method="post" action="admin.php?page=<?=$this->plugin?>-plugin&amp;tab=console">
+                <?php
+                    settings_fields($this->prefix . 'console');
+                    do_settings_sections($this->prefix . 'console');
+                ?>
+            </form>
+        <?php endif; ?>
+        </div>
+    <?php
+    }
+
+    /**
+     * OPTIONS TAB
+     * 
+     * @since 1.0
+    */
+    public function options_page_fields()
+    {
+        add_settings_section($this->prefix . 'settings', __('Settings', $this->plugin), null, $this->prefix . 'settings');
+
+        // add_settings_field($this->prefix . "enabled", __("Enable" , $this->plugin), array($this, $this->prefix . "enabled"), $this->prefix . 'settings', $this->prefix . 'settings');
+        add_settings_field($this->prefix . "ips", __("Servers IPs", $this->plugin), array($this, $this->prefix . "ips"), $this->prefix . 'settings', $this->prefix . 'settings');
+        // add_settings_field($this->prefix . "additional_domains", __("Additional domains", $this->plugin), array($this, $this->prefix . "ips"), $this->prefix . 'settings', $this->prefix . 'settings');
+        // add_settings_field($this->prefix . "purge_key", __("Purge key", $this->plugin), array($this, $this->prefix . "purge_key"), $this->prefix . 'settings', $this->prefix . 'settings');
+        // add_settings_field($this->prefix . "truncate_notice", __("Truncate notice message", $this->plugin), array($this, $this->prefix . "truncate_notice"), $this->prefix . 'settings', $this->prefix . 'settings');
+        add_settings_field($this->prefix . "use_purge_method", __("Use PURGE HTTP method", $this->plugin), array($this, $this->prefix . "use_purge_method"), $this->prefix . 'settings', $this->prefix . 'settings');
+        add_settings_field($this->prefix . "debug", __("Enable debug", $this->plugin), array($this, $this->prefix . "debug"), $this->prefix . 'settings', $this->prefix . 'settings');
+        add_settings_field($this->prefix . "purge_path", __("PURGE path", $this->plugin), array($this, $this->prefix . "purge_path"), $this->prefix . 'settings', $this->prefix . 'settings');
+
+        // add_settings_section($this->prefix . 'settings_use_purge', __('Purge Settings', $this->plugin), null, $this->prefix . 'settings');
+
+        if(isset($_POST['option_page']) && $_POST['option_page'] == $this->prefix . 'settings') {
+            // register_setting($this->prefix . 'settings', $this->prefix . "enabled");
+            register_setting($this->prefix . 'settings', $this->prefix . "ips");
+            // register_setting($this->prefix . 'settings', $this->prefix . "purge_key");
+            // register_setting($this->prefix . 'settings', $this->prefix . "truncate_notice");
+            register_setting($this->prefix . 'settings', $this->prefix . "use_purge_method", true);
+            register_setting($this->prefix . 'settings', $this->prefix . "purge_path");
+            // register_setting($this->prefix . 'settings', $this->prefix . "additional_domains");
+            register_setting($this->prefix . 'settings', $this->prefix . "debug");
+        }
+
+    }
+    
+    /**
+    * @since 1.0
+    */
+    public function remote_cache_ips()
+    {
+        ?>
+            <input type="text" name="remote_cache_ips" id="remote_cache_ips" size="100" value="<?php echo get_option($this->prefix . 'ips'); ?>" />
+            <p class="description"><?=__('Comma separated ip/ip:port. Example: 192.168.0.2,192.168.0.3:8080', $this->plugin)?></p>
+        <?php
+    }
+
+    /**
+    * @since 1.0
+    */
+    public function remote_cache_debug()
+    {
+        ?>
+            <input type="checkbox" name="remote_cache_debug" value="1" <?php checked(1, get_option($this->prefix . 'debug'), true); ?> />
+            <p class="description">
+                <?=__('Send logging data to WP debug.log.', $this->plugin)?>
+            </p>
+        <?php
+    }
+
+    
+    /**
+    * @since 1.0.1
+    */
+    public function remote_cache_use_purge_method()
+    {
+        ?>
+            <input type="checkbox" name="remote_cache_use_purge_method" value="1" <?php checked(1, get_option($this->prefix . 'use_purge_method'), true); ?> />
+            <p class="description">
+                <?=__('Use PURGE http method or /purge(/.*) path', $this->plugin)?>
+            </p>
+        <?php
+    }
+
+    /**
+    * @since 1.0.1
+    */
+    public function remote_cache_purge_path()
+    {
+        ?>
+            <input type="text" name="remote_cache_purge_path" id="remote_cache_purge_path" size="100" value="<?php echo get_option($this->prefix . 'purge_path'); ?>" />
+            <p class="description"><?=__('If your\'re using purge path enter your path here, based on your server configuration, Example: /purge', $this->plugin)?></p>
+        <?php
+    }
+
+    /**
+     * END OF OPTIONS TAB
+    */
+
+    /**
+     *  
+     * CONSOLE TAB
+     * 
+     * @since 1.0
+    */
+    public function console_page_fields()
+    {
+        add_settings_section('console', __("Console", $this->plugin), null, $this->prefix . 'console');
+
+        add_settings_field($this->prefix . "purge_url", __("URL", $this->plugin), array($this, $this->prefix . "purge_url"), $this->prefix . 'console', "console");
+    }
+
+    /**
+    * @since 1.0
+    */
+    public function remote_cache_purge_url()
+    {
+        ?>
+            <input type="text" name="remote_cache_purge_url" size="100" id="remote_cache_purge_url" value="" />
+            <a href="javascript:;" id="remote-cache-purger-purge-link">Purge</a>
+            <p class="description"><?=__('Relative URL to purge. Example : /simple-post or /uncategorized/hello-world. It will clear that URL page (and related pages) cache on ALL reported servers', $this->plugin)?></p>
+        <?php
+    }
+
+    /**
+     * END OF CONSOLE TAB
+    */
+
+    /**
     * @since 1.0.1
     */
 	public function admin_footer( $text ) {
--- remote-cache-purger.php
+++ remote-cache-purger.php
@@ -4,7 +4,7 @@
      * Description: Clearing cache on remote NGINX servers (Kubernetes)
      * Author: Myros
      * Author URI: https://www.myros.net/
-     * Version: 1.0.1
+     * Version: 1.0.2
      
      * License: http://www.apache.org/licenses/LICENSE-2.0
      * Text Domain: remote-cache-purger
@@ -41,8 +41,8 @@ class Main {
     // general
 
     protected $blogId; 
-    protected $plugin = 'cache-purger';
-    protected $prefix = 'remote_cache_'; // remote cache purger
+    public $plugin = 'remote-cache-purger';
+    public $prefix = 'remote_cache_'; // remote cache purger
 
     protected $serverIPS = array();
     protected $getParam = 'purge_remote_cache';
@@ -94,8 +94,6 @@ class Main {
         add_action('wp_ajax_remote_cache_purge_all', array( $this, 'purgejs' ) );
         add_action('wp_ajax_remote_cache_purge_item', array( $this, 'purgejs_item' ) );
         add_action('wp_ajax_remote_cache_purge_url', array( $this, 'purgejs_url' ) );
-
-        
     }
 
     /**
@@ -103,7 +101,7 @@ class Main {
     */
     public function init()
     {
-      $this->write_log('Init', 'Start');
+      $this->write_log('Main', 'Init', 'Start');
 
       load_plugin_textdomain( self::NAME, false, self::NAME . '/languages' );
 
@@ -116,7 +114,6 @@ class Main {
       $this->postTypes = get_post_types(array('show_in_rest' => true));
 
       $this->loadOptions();
-      $this->admin_menu();
 
       // register events to purge post
       foreach ($this->registeredEvents as $event) {
@@ -132,8 +129,8 @@ class Main {
         add_filter('page_row_actions', array(&$this, 'page_row_actions'), 0, 2);
       }
 
-      $this->currentTab = isset($_GET['tab']) ? $_GET['tab'] : 'settings';
-      $this->write_log('Init', 'End');
+    //   $this->currentTab = isset($_GET['tab']) ? $_GET['tab'] : 'settings';
+      $this->write_log('Main', 'Init', 'End');
     }
         
     /**
@@ -456,93 +453,6 @@ class Main {
 
         return true;
     }
-    
-    /**
-    * @since 1.0
-    */
-    public function admin_menu()
-    {
-        add_action('admin_menu', array($this, 'add_menu_item'));
-        add_action('admin_init', array($this, 'options_page_fields'));
-        add_action('admin_init', array($this, 'console_page_fields'));
-    }
-
-    /**
-    * @since 1.0
-    */
-    public function add_menu_item()
-    {
-        if ($this->check_if_purgeable()) {
-            add_menu_page(__('Remote Cache Purger', $this->plugin), __('Remote Cache Purger', $this->plugin), 'manage_options', $this->plugin . '-plugin', array($this, 'settings_page'), 'dashicons-sos', 99);
-        }
-    }
-
-    /**
-    * @since 1.0
-    */
-    public function settings_page()
-    {
-    ?>
-        <div class="wrap">
-        <h1><?=__('Remote Cache Purger', $this->plugin)?></h1>
-
-        <h2 class="nav-tab-wrapper">
-            <a class="nav-tab <?php if($this->currentTab == 'settings'): ?>nav-tab-active<?php endif; ?>" href="<?php echo admin_url() ?>index.php?page=<?=$this->plugin?>-plugin&amp;tab=settings"><?=__('Settings', $this->plugin)?></a>
-            <?php if ($this->check_if_purgeable()): ?>
-                <a class="nav-tab <?php if($this->currentTab == 'console'): ?>nav-tab-active<?php endif; ?>" href="<?php echo admin_url() ?>index.php?page=<?=$this->plugin?>-plugin&amp;tab=console"><?=__('Console', $this->plugin)?></a>
-            <?php endif; ?>
-        </h2>
-
-        <?php if($this->currentTab == 'settings'): ?>
-            <form method="post" action="options.php">
-                <?php
-                    settings_fields($this->prefix . 'settings');
-                    do_settings_sections($this->prefix . 'settings');
-                    submit_button();
-                ?>
-            </form>
-        <?php elseif($this->currentTab == 'console'): ?>
-            <form method="post" action="admin.php?page=<?=$this->plugin?>-plugin&amp;tab=console">
-                <?php
-                    settings_fields($this->prefix . 'console');
-                    do_settings_sections($this->prefix . 'console');
-                ?>
-            </form>
-        <?php endif; ?>
-        </div>
-    <?php
-    }
-
-    /**
-    * @since 1.0
-    */
-    public function options_page_fields()
-    {
-        add_settings_section($this->prefix . 'settings', __('Settings', $this->plugin), null, $this->prefix . 'settings');
-
-        // add_settings_field($this->prefix . "enabled", __("Enable" , $this->plugin), array($this, $this->prefix . "enabled"), $this->prefix . 'settings', $this->prefix . 'settings');
-        add_settings_field($this->prefix . "ips", __("IPs", $this->plugin), array($this, $this->prefix . "ips"), $this->prefix . 'settings', $this->prefix . 'settings');
-        // add_settings_field($this->prefix . "additional_domains", __("Additional domains", $this->plugin), array($this, $this->prefix . "ips"), $this->prefix . 'settings', $this->prefix . 'settings');
-        // add_settings_field($this->prefix . "purge_key", __("Purge key", $this->plugin), array($this, $this->prefix . "purge_key"), $this->prefix . 'settings', $this->prefix . 'settings');
-        // add_settings_field($this->prefix . "truncate_notice", __("Truncate notice message", $this->plugin), array($this, $this->prefix . "truncate_notice"), $this->prefix . 'settings', $this->prefix . 'settings');
-        add_settings_field($this->prefix . "use_purge_method", __("Use PURGE method", $this->plugin), array($this, $this->prefix . "use_purge_method"), $this->prefix . 'settings', $this->prefix . 'settings');
-        // add_settings_field($this->prefix . "debug", __("Enable debug", $this->plugin), array($this, $this->prefix . "debug"), $this->prefix . 'settings', $this->prefix . 'settings');
-        add_settings_field($this->prefix . "purge_path", __("PURGE path", $this->plugin), array($this, $this->prefix . "purge_path"), $this->prefix . 'settings', $this->prefix . 'settings');
-
-        // add_settings_section($this->prefix . 'settings_use_purge', __('Purge Settings', $this->plugin), null, $this->prefix . 'settings');
-
-        if(isset($_POST['option_page']) && $_POST['option_page'] == $this->prefix . 'settings') {
-            // register_setting($this->prefix . 'settings', $this->prefix . "enabled");
-            register_setting($this->prefix . 'settings', $this->prefix . "ips");
-            // register_setting($this->prefix . 'settings', $this->prefix . "purge_key");
-            // register_setting($this->prefix . 'settings', $this->prefix . "truncate_notice");
-            register_setting($this->prefix . 'settings', $this->prefix . "use_purge_method", true);
-            register_setting($this->prefix . 'settings', $this->prefix . "purge_path");
-            // register_setting($this->prefix . 'settings', $this->prefix . "additional_domains");
-            // register_setting($this->prefix . 'settings', $this->prefix . "debug");
-        }
-
-    }
 
     /**
     * @since 1.0
@@ -555,16 +465,6 @@ class Main {
         <?php
     }
 
-    /**
-    * @since 1.0
-    */
-    public function remote_cache_ips()
-    {
-        ?>
-            <input type="text" name="remote_cache_ips" id="remote_cache_ips" size="100" value="<?php echo get_option($this->prefix . 'ips'); ?>" />
-            <p class="description"><?=__('Comma separated ip/ip:port. Example: 192.168.0.2,192.168.0.3:8080', $this->plugin)?></p>
-        <?php
-    }
 
     /**
     * @since 1.0
@@ -579,35 +479,7 @@ class Main {
         <?php
     }
 
-    /**
-    * @since 1.0.1
-    */
-    public function remote_cache_use_purge_method()
-    {
-        ?>
-            <input type="checkbox" name="remote_cache_use_purge_method" value="1" <?php checked(1, get_option($this->prefix . 'use_purge_method'), true); ?> />
-            <p class="description">
-                <?=__('Use PURGE http method or /purge(/.*) path', $this->plugin)?>
-            </p>
 
-            <script type="text/javascript">
-                function togglePurge() {
-                    jQuery('#' + id).val(outStr);
-                }
-            </script>
-        <?php
-    }
-
-    /**
-    * @since 1.0.1
-    */
-    public function remote_cache_purge_path()
-    {
-        ?>
-            <input type="text" name="remote_cache_purge_path" id="remote_cache_purge_path" size="100" value="<?php echo get_option($this->prefix . 'purge_path'); ?>" />
-            <p class="description"><?=__('If your\'re using purge path enter your path here, based on your server configuration, Example: /purge', $this->plugin)?></p>
-        <?php
-    }
 
     /**
     * @since 1.0
@@ -635,40 +507,8 @@ class Main {
         <?php
     }
 
-    /**
-    * @since 1.0
-    */
-    public function remote_cache_debug()
-    {
-        ?>
-            <input type="checkbox" name="remote_cache_debug" value="1" <?php checked(1, get_option($this->prefix . 'debug'), true); ?> />
-            <p class="description">
-                <?=__('Send all debugging headers to the client. Also shows complete response from Remote Server on purge all.', $this->plugin)?>
-            </p>
-        <?php
-    }
 
-    /**
-    * @since 1.0
-    */
-    public function console_page_fields()
-    {
-        add_settings_section('console', __("Console", $this->plugin), null, $this->prefix . 'console');
 
-        add_settings_field($this->prefix . "purge_url", __("URL", $this->plugin), array($this, $this->prefix . "purge_url"), $this->prefix . 'console', "console");
-    }
-
-    /**
-    * @since 1.0
-    */
-    public function remote_cache_purge_url()
-    {
-        ?>
-            <input type="text" name="remote_cache_purge_url" size="100" id="remote_cache_purge_url" value="" />
-            <a href="javascript:;" id="remote-cache-purger-purge-link">Purge</a>
-            <p class="description"><?=__('Relative URL to purge. Example : /simple-post or /uncategorized/hello-world. It will clear that URL page (and related pages) cache on ALL reported servers', $this->plugin)?></p>
-        <?php
-    }
     // end of console
 
     /**
@@ -725,7 +565,6 @@ class Main {
             }
         }
     }
-    
 }
 
 \RemoteCachePurger\Main::getInstance();
