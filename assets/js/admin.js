a285b221306d0e004be31eeed9ea7135efcc8a86
Myros <miroslav.milak@gmail.com>
2020-12-23 21:33:30 -0500

    Optimize JS

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,9 +1,8 @@
 (function ($) {
+	
 	$(function () {
-
-		// purge all from admin bar
-		$('#wp-admin-bar-purge-all-remote-cache a, #wp-glance-purge-all-remote-cache a').click(function (e) {
-			e.preventDefault();
+		
+		function executeAction(action, second) {
 			
 			var d = new Date();
 			var n = d.getMilliseconds();
@@ -17,7 +16,9 @@
 			$.post(
 				ajaxurl,
 				{
-					action: 'remote_cache_purge_all',
+					id: second,
+					url: second,
+					action: action,
 					wp_nonce: $.trim($('#remote-cache-purger-purge-wp-nonce').text())
 				},
 				function (r) {
@@ -38,99 +39,32 @@
 					notice.on('click', function () {
 						$(this).remove();
 					});
-					notice.delay(5000).fadeOut();
+					notice.delay(5000).fadeOut(function() { 
+						$(this).fadeOut().remove(); 
+					});
 				}
 			);
+
+		}
+		
+		// purge all from admin bar
+		$('#wp-admin-bar-purge-all-remote-cache a, #wp-glance-purge-all-remote-cache a').click(function (e) {
+			e.preventDefault();
+			executeAction('remote_cache_purge_all')
 		});
 
 		// purge url from settings console page
 		$('#remote-cache-purger-purge-link').click(function (e) {
 			e.preventDefault();
-
-			var d = new Date();
-			var n = d.getMilliseconds();
-			
-			var container = $('#remote-cache-purger-admin-notices');
 			var url = $('#remote_cache_purge_url').val();
-			// ugly hack to create a container div for our notices in the right location
-			container.removeClass('notice').show();
-			var noticeId = 'remote-purger-notice-' + n;
-			var notice = $('<div class="notice" id="' + noticeId + '"></div>');
-			notice.html('<p>Started purging</p>')
-			container.append(notice);
-			$.post(
-				ajaxurl,
-				{
-					url: url,
-					action: 'remote_cache_purge_url',
-					wp_nonce: $.trim($('#remote-cache-purger-purge-wp-nonce').text())
-				},
-				function (r) {
-					try {
-						var response = JSON.parse(r);
-					} catch (error) {
-						var response = {success: false, message: error}
-					}
-					var noticeClass = 'notice-success';
-					if (!response.success) {
-						noticeClass = 'notice-error';
-					}
-					notice.toggleClass(noticeClass);
-					notice.html('<p>' + response.message + '</p>')
-					container.empty().append(notice);
-					notice.on('click', function () {
-						$(this).remove();
-					});
-					notice.delay(5000).fadeOut();
-				}
-			);
+			executeAction('remote_cache_purge_url', url);
 		});
 
 		// purge one item
 		$('.rcpurger_purge_post a').click(function (e) {
 			e.preventDefault();
-
-			var d = new Date();
-			var n = d.getMilliseconds();
-
 			var id = $(this).attr('data-item-id');
-			var container = $('#remote-cache-purger-admin-notices');
-			// ugly hack to create a container div for our notices in the right location
-			container.removeClass('notice').show();
-			console.log('lic')
-			var noticeId = 'remote-purger-notice-' + n;
-			console.log(noticeId)
-			var notice = $('<div class="notice" id="' + noticeId + '"><p>Started purging</p></div>');
-			notice.html('<p>Started purging</p>')
-
-			container.append(notice);
-			$.post(
-				ajaxurl,
-				{
-					id: id,
-					action: 'remote_cache_purge_item',
-					wp_nonce: $.trim($('#remote-cache-purger-purge-wp-nonce').text())
-					
-				},
-				function (r) {
-					try {
-						var response = JSON.parse(r);
-					} catch (error) {
-						var response = {success: false, message: error}
-					}
-					var noticeClass = 'notice-success';
-					if (!response.success) {
-						noticeClass = 'notice-error';
-					}
-					var elNotice = $('#' + noticeId)
-					notice.toggleClass(noticeClass);
-					notice.html('<p>' + response.message + '</p>')
-					notice.on('click', function () {
-						$(this).fadeOut();
-					});
-					notice.delay(5000).fadeOut();
-				}
-			);
+			executeAction('remote_cache_purge_item', id);
 		});
 	});
 }(jQuery));
